2d2d28539effa21a12c45da85312a405b3db0162
RodofoF <89172848+RodofoF@users.noreply.github.com>
2023-02-17 11:59:14 -0300

    Inserido autenticação

--- routes/toughtRoutes.js
+++ routes/toughtRoutes.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 const ThoughtController = require('../controllers/toughtController')
 
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/add',checkAuth, ThoughtController.createTought)
+router.post('/add',checkAuth, ThoughtController.createToughtSave)
+router.get('/dashboard',checkAuth, ThoughtController.dashboard)
 router.get('/',ThoughtController.showToughts)
+
 
 module.exports = router
